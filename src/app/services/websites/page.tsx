3cfca07fc73fa98ffc8db2251e9d7e37779a450d
Collin Vossman <cavossman@gmail.com>
2024-11-12 23:20:10 -0600

    style content

--- src/app/services/websites/page.tsx
+++ src/app/services/websites/page.tsx
@@ -31,75 +31,139 @@ const WebsitesPage = () => {
           solutions that meet your unique business needs, ensuring that your
           online store stands out in the competitive digital marketplace.
         </p>
-        <h3>WordPress with WooCommerce</h3>
-        <p>
-          For businesses seeking the flexibility and scalability of WordPress,
-          we offer expert development services using WooCommerce. This powerful
-          plugin allows you to turn your WordPress site into a fully functional
-          eCommerce platform. With WooCommerce, you can enjoy a range of
-          features including product management, inventory tracking, secure
-          payments, and more. Our team will customize your WooCommerce store to
-          meet your specific requirements, ensuring a seamless integration with
-          your WordPress site.
-        </p>
-        <h3>Benefits of WooCommerce:</h3>
-        <ul>
-          <li>
-            <strong>Flexibility:</strong> Customize your store with a wide range
-            of themes and plugins.
-          </li>
-          <li>
-            <strong>Scalability:</strong> Grow your store with ease as your
-            business expands.
-          </li>
-          <li>
-            <strong>SEO-Friendly:</strong> Benefit from WordPress&apos;s
-            powerful SEO capabilities.
-          </li>
-          <li>
-            <strong>Community Support:</strong> Access a large community of
-            developers and resources.
-          </li>
-        </ul>
 
-        <h2>Shopify</h2>
+        <div className="grid lg:grid-cols-2 gap-8 my-12 bg-dark-light rounded-lg p-4">
+          <h2 className="col-span-2 mb-0">WordPress</h2>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <p>
+              For businesses seeking the flexibility and scalability of
+              WordPress, we offer expert development services using WooCommerce.
+              This powerful plugin allows you to turn your WordPress site into a
+              fully functional eCommerce platform. With WooCommerce, you can
+              enjoy a range of features including product management, inventory
+              tracking, secure payments, and more. Our team will customize your
+              WooCommerce store to meet your specific requirements, ensuring a
+              seamless integration with your WordPress site.
+            </p>
+          </div>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <h3>Benefits:</h3>
+            <ul>
+              <li>
+                <strong>Flexible:</strong> Customize your store with a wide
+                range of themes and plugins.
+              </li>
+              <li>
+                <strong>Scalability:</strong> Grow your store with ease as your
+                business expands.
+              </li>
+              <li>
+                <strong>SEO-Friendly:</strong> Benefit from WordPress&apos;s
+                powerful SEO capabilities.
+              </li>
+              <li>
+                <strong>Community Support:</strong> Access a large community of
+                developers and resources.
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="grid lg:grid-cols-2 gap-8 my-12 bg-dark-light rounded-lg p-4">
+          <h2 className="col-span-2 mb-0">Shopify</h2>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <p>
+              If you prefer a more streamlined and user-friendly platform,
+              Shopify is an excellent choice. Known for its ease of use and
+              robust functionality, Shopify enables you to set up and manage
+              your online store with minimal hassle. Our team of Shopify experts
+              will design and develop a custom store that meets your needs, from
+              product listings and payment processing to inventory management
+              and customer support.
+            </p>
+          </div>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <h3>Benefits:</h3>
+            <ul>
+              <li>
+                <strong>Ease of Use:</strong> Simple setup and management with
+                an intuitive interface.
+              </li>
+              <li>
+                <strong>Security:</strong> Reliable and secure payment gateways.
+              </li>
+              <li>
+                <strong>Mobile-Friendly:</strong> Responsive designs that look
+                great on all devices.
+              </li>
+              <li>
+                <strong>App Integrations:</strong> Enhance your store with a
+                wide range of apps and tools.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <h2 className="mt-32">
+          Streamline Your Business with Back Office Solutions
+        </h2>
         <p>
-          If you prefer a more streamlined and user-friendly platform, Shopify
-          is an excellent choice. Known for its ease of use and robust
-          functionality, Shopify enables you to set up and manage your online
-          store with minimal hassle. Our team of Shopify experts will design and
-          develop a custom store that meets your needs, from product listings
-          and payment processing to inventory management and customer support.
+          In today&apos;s fast-paced digital landscape, back office solutions
+          are key to improving business workflows and automating repetitive
+          processes. From accounting and human resources to inventory and
+          customer service, these solutions help companies operate more
+          efficiently and minimize manual work. By integrating tailored software
+          and leveraging automation, businesses can reduce errors, save time,
+          and enable teams to focus on more strategic, growth-oriented
+          activities. Whether it&apos;s through ERP systems, CRM software, or
+          automated payroll, back office solutions create a solid foundation for
+          smoother, more effective operations.
         </p>
 
-        <h3>Benefits of Shopify:</h3>
-        <ul>
-          <li>
-            <strong>Ease of Use:</strong> Simple setup and management with an
-            intuitive interface.
-          </li>
-          <li>
-            <strong>Security:</strong> Reliable and secure payment gateways.
-          </li>
-          <li>
-            <strong>Mobile-Friendly:</strong> Responsive designs that look great
-            on all devices.
-          </li>
-          <li>
-            <strong>App Integrations:</strong> Enhance your store with a wide
-            range of apps and tools.
-          </li>
-        </ul>
+        <div className="grid lg:grid-cols-2 gap-8 my-12 bg-dark-light rounded-lg p-4 mb-0">
+          <h2 className="col-span-2 mb-0">Laravel</h2>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <p>
+              Laravel is a powerful framework that enables businesses to build
+              custom internal tools, tailored to streamline unique workflows and
+              enhance productivity. With Laravel&apos;s flexibility, we can
+              develop applications that handle anything from data management and
+              reporting to user permissions and automated processes. Our team
+              specializes in creating secure, efficient tools that are easy to
+              use and scale as your business grows.
+            </p>
+          </div>
+          <div className="col-span-2 lg:col-span-1 bg-dark rounded-lg p-4">
+            <h3>Benefits:</h3>
+            <ul>
+              <li>
+                <strong>Customization:</strong> Tailored to fit your specific
+                business needs and processes.
+              </li>
+              <li>
+                <strong>Security:</strong> Built-in features to secure data and
+                restrict access based on user roles.
+              </li>
+              <li>
+                <strong>Automation:</strong> Automate repetitive tasks, freeing
+                up time for higher-value work.
+              </li>
+              <li>
+                <strong>Integration:</strong> Easily integrate with other
+                systems, such as CRMs or payment gateways.
+              </li>
+            </ul>
+          </div>
+        </div>
 
-        <h2>Transform Your Business</h2>
+        {/* <h2>Transform Your Business</h2>
         <p>
-          At CodeVoss, we’re committed to helping you succeed in the digital
-          marketplace. Our expertise in WordPress and Shopify allows us to
-          deliver customized, user-friendly, and efficient eCommerce solutions
-          that transform your business and boost your success. Let us help you
-          create an online store that not only looks impressive but also drives
-          sales and supports your growth.
-        </p>
+          At CodeVoss, we&apos;re committed to helping you succeed in the
+          digital marketplace. Our expertise in WordPress and Shopify allows us
+          to deliver customized, user-friendly, and efficient eCommerce
+          solutions that transform your business and boost your success. Let us
+          help you create an online store that not only looks impressive but
+          also drives sales and supports your growth.
+        </p> */}
       </section>
       <OurProcess />
       <section className="container content">
